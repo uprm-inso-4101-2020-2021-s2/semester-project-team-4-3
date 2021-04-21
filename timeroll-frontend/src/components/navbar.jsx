4ff8d21ac34de6a4c4592e1120a9a0f5e5bb2c96
Serrgust <gustavo.serrano2@upr.edu>
2021-04-20 21:21:42 -0400

    can login and logout

--- timeroll-frontend/src/components/navbar.jsx
+++ timeroll-frontend/src/components/navbar.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import '../Menu.js'
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 //Stateless Functional Component
 
@@ -13,8 +17,24 @@ import React, { Component } from 'react';
 // };
 
 class NavBar extends Component {
+
+    signOut=()=>{
+        cookies.remove('id', {path: "/"});
+        cookies.remove('Last_name', {path: "/"});
+        cookies.remove('Name', {path: "/"});
+        cookies.remove('username', {path: "/"});
+        window.location.href='./';
+    }
+
+    componentDidMount() {
+        if(!cookies.get('username')){
+            window.location.href="./";
+        }
+    }
+
     //state = {  }
     render() {
+        
         return (<nav className="navbar navbar-dark bg-dark navbar-expand-sm shadow">
             <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3">
                 TimeRoll
@@ -25,10 +45,14 @@ class NavBar extends Component {
                     <a className="nav-link"> Employee </a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link"> Sign out</a>
+                    {/* <a className="nav-link"> Sign out</a> */}
+                    <button onClick={()=>this.signOut()}>Sign out</button>
+                    
                 </li>
             </ul>
+            
         </nav>);
+        
     }
 }
 
